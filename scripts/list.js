c9483a8bc832c933aa39d8395e4323779141589f
vaibhav rajput <truemusclescience@gmail.com>
2022-06-22 12:14:08 +0530

    list hotel bug fixing

--- scripts/list.js
+++ scripts/list.js
@@ -25,72 +25,71 @@ function listbtn2() {
 
 
 
+var place = location.search;
 
 
-//   let city2 = hh;
-//   let locat = `https://travel-advisor.p.rapidapi.com/locations/search?query=${city2}&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_Delhi`;    
   
-
-
-
-// console.log("city" + city2);
-
-// const options = {
-//      method: 'GET',
-//      headers: {
-//           'X-RapidAPI-Key': '87bba50f70mshfabbd820e4160ddp101ebejsnccbf1bb41df7',
-//           'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-//      }
-// };
-
-
-// fetch(locat, options)
-//      .then(response => response.json())
-//      .then((response)=>{
-//     let hotel = response.data;
-//     let html = "";
-//    hotel.map((values)=>{
-
-//     let img1 = values.result_object.photo.images.medium.url;
-//     let hotelname = values.result_object.name;
-//     let hoteladress = values.result_object.address;
-//     let hotelrating = values.result_object.rating;
-//     // console.log(hoteladress);
-//     // console.log(img1);
-//     html += `
-//     <div class="grid-item">   
-//         <img src=${img1} alt=""
-//             class="hotel">
-//         <div class="rating">
-//             <h2>${hotelname}</h2>
-//             <div class="stars">
-//                <span >${hotelrating}</span>
-//                 <span class="star checked">&#9733;</span>
-                
-                
-//                 <br>
-//                 <br>
-//                 <p>${hoteladress}</p>
-//             </div>
-
-//         </div>
+  let locat = `https://travel-advisor.p.rapidapi.com/locations/search?query=${place}&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_Delhi`;    
+  
+  const options = {
+    method: 'GET',
+    headers: {
+         'X-RapidAPI-Key': '87bba50f70mshfabbd820e4160ddp101ebejsnccbf1bb41df7',
+         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+    }
+  };
+  
+  
+  fetch(locat, options)
+    .then(response => response.json())
+    .then((response)=>{
+     let hotel = response.data;
+     
+    // console.log(hotel);
+      var html =" ";
+       hotel.map((values)=>{
+        
+        let img1 = values.result_object.photo.images.medium.url;
+        let hotelname = values.result_object.name;
+        let hoteladress = values.result_object.address;
+        let hotelrating = values.result_object.rating;
+        //  console.log(hoteladress);
+        //  console.log(img1);
+        html += `
+        <div class="grid-item">   
+            <img src=${img1} alt=""
+                class="hotel">
+            <div class="rating">
+                <h2>${hotelname}</h2>
+                <div class="stars">
+                   <span >${hotelrating}</span>
+                    <span class="star checked">&#9733;</span>
+                    
+                    
+                    <br>
+                    <br>
+                    <p>${hoteladress}</p>
+                </div>
     
-
-//     </div>`
-
-//    })
-//   document.getElementById("contentlist").innerHTML = html;
-//   console.log(response.data)
-
-
-
-
-
-// }).catch(err => console.error(err));
-
-let you = document.getElementById("contentlist").innerHTML;
-console.log(you);
-
-
-
-
+            </div>
+        
+    
+        </div>`
+    
+       })
+  
+      
+       
+      document.getElementById("contentlist").innerHTML = html;
+      console.log(response.data)
+    
+    
+    
+  
+  }).catch(err => console.error(err));
+  
+  
+  
+  
+  
+   
